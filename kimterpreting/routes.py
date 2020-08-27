98d39e1d7f85289a4803a1fe6d17b52d3aaf55db
Sam Helman <07shelman@gmail.com>
2020-08-27 14:20:00 -0400

    Initial commit. Finished the main title page and about page with animations.

---
+++ kimterpreting/routes.py
@@ -0,0 +1,22 @@
+from kimterpreting import app
+from flask import render_template
+
+@app.route('/')
+def home_animations():
+  return render_template('home.html', animations=True)
+
+@app.route('/home')
+def home():
+  return render_template('home.html', title="Home", animations=False)
+
+@app.route('/contact')
+def contact():
+  return "Contact"
+
+@app.route('/faq')
+def faq():
+  return "FAQ"
+
+@app.route('/terms')
+def terms():
+  return "Terms"
